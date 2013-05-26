d3f15668369d6c375a0dc38c0c5836747bd3b5ac
Masaaki Kikuchi <masa21kik@gmail.com>
2013-05-27 00:07:58 +0900

    implement fizzbuzz.

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,5 +2,9 @@
 
 class FizzBuzz
   def say(number)
+    word = ''
+    word << 'fizz' if number % 3 == 0
+    word << 'buzz' if number % 5 == 0
+    return word.empty? ? number.to_s : word
   end
 end
